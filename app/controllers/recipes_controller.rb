6c897783470a3751a4e001cc7f1f623906d201e6
izyszka <iza.wisniewska.iza@gmail.com>
2020-01-19 23:53:50 +0100

    Generate MVC, set routes and define CRUD actions for recipes

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,49 @@
+class RecipesController < ApplicationController
+  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(params[:recipe].permit(:title, :description))
+
+    if @recipe.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @recipe.update(params[:recipe].permit(:title, :description))
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @recipe.destroy
+      redirect_to root_path
+    else
+      redirect_to @recipe
+    end
+  end
+
+  private
+
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+end
